f790af4d37838decd78fcb955f745ad43534a0de
ankit90s <ak7591527@gmail.com>
2023-04-03 18:46:25 +0530

    bug fixes

---
+++ app.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+## check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
+
+# Get username and make builddir
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Apps of choice
+apt install -y pip ranger cmatrix espeak ncdu translate-shell rsync kdeconnect yt-dlp gpick
+
+# Browser of choice
+apt install -y firefox-esr
+
+# Torrent client
+apt install -y transmission-qt
+
+# sensors
+apt install -y lm-sensors hddtemp
+
+# Archiving tools
+apt install -y zip unzip p7zip-full
+
+# search applications
+apt install -y rofi dmenu
+
+# System Monitors
+apt install -y conky neofetch htop
+
+# Calculator
+apt install -y gnome-calculator
+
+# Download Managers
+apt install -y curl wget axel aria2
+
+# Network File Tools/System Events
+apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends locate mtp-tools
+systemctl enable avahi-daemon
+systemctl enable acpid
+
+# redshift
+apt install -y redshift
+
+# Arc theme
+apt install -y arc-theme
+
+# Text editor
+apt install -y neovim geany
+git clone https://github.com/VundleVim/Vundle.vim.git /home/$username/.vim/bundle/Vundle.vim
+
+# Papirus icon
+apt install -y papirus-icon-theme -t bullseye-backports
+
+# Papirus folder
+wget -qO- https://git.io/papirus-folders-install | sh
+papirus-folders -C yaru --theme Papirus-Dark
+
+# Kvantum
+apt install -y qt5-style-kvantum
+
+# WhiteSur Icon Theme
+cd /home/$username
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd WhiteSur-icon-theme
+mkdir -p /home/$username/.local/share/icons
+./install.sh -d /home/$username/.local/share/icons
+cd .. && rm -rf WhiteSur-icon-theme
+cd builddir
+
+# Mc Mojave-kde Theme
+git clone https://github.com/vinceliuice/McMojave-kde.git
+cd McMojave-kde
+./install.sh -d /usr/share/themes
+cd .. && rm -rf McMojave-kde
+
+# Mojave gtk theme
+apt install -y gtk2-engines-murrine gtk2-engines-pixbuf sassc optipng inkscape libglib2.0-dev-bin libglib2.0-dev libxml2-utils
+git clone https://github.com/vinceliuice/Mojave-gtk-theme.git
+cd Mojave-gtk-theme
+./install.sh -d /usr/share/themes
+cd .. && rm -rf Mojave-gtk-theme
+
+# Restricted extras
+apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+
+# Alacritty Dependencies
+apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+# libreoffice install
+apt install -y libreoffice-gtk3 libreoffice-impress libreoffice-calc libreoffice-writer
+
+# libreoffice sifr theme
+curl -s https://raw.githubusercontent.com/rizmut/libreoffice-style-sifr/master/install-sifr.sh | sh
+
+# install nvidia detect
+apt install -y nvidia-detect
+
+# install build tools
+apt install -y build-essential linux-headers-$(uname -r)
+
+# config files
+cd /home/$username
+git clone https://github.com/ankit90s/dotconfig && cd dotconfig
+cp -r * /home/$username/.config
+chown -R $username:$username /home/$username
+cd $builddir
+
+echo 
+echo Reboot and Enjoy
+echo
--- lxqt.sh
+++ lxqt.sh
@@ -18,88 +18,8 @@ builddir=$(pwd)
 # install lxqt desktop
 apt install -y lxqt-core
 
-# Apps of choice
-apt install -y pip ranger cmatrix espeak ncdu translate-shell rsync kdeconnect yt-dlp gpick
-
 # Exa Installation
 apt install -y exa
-
-# terminal of choice
-apt install -y xterm terminator
-
-# Browser of choice
-apt install -y firefox-esr
-
-# Torrent client
-apt install -y transmission-qt
-
-# sensors
-apt install -y lm-sensors hddtemp
-
-# Archiving tools
-apt install -y zip unzip p7zip-full
-
-# search applications
-apt install -y rofi dmenu
-
-# System Monitors
-apt install -y conky neofetch htop
-
-# Calculator
-apt install -y gnome-calculator
-
-# Download Managers
-apt install -y curl wget axel aria2
-
-# Network File Tools/System Events
-apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends locate mtp-tools
-
-systemctl enable avahi-daemon
-systemctl enable acpid
-
-# redshift
-apt install -y redshift
-
-# Arc theme
-apt install -y arc-theme
-
-# Text editor
-apt install -y neovim geany
-git clone https://github.com/VundleVim/Vundle.vim.git /home/$username/.vim/bundle/Vundle.vim
-
-# Papirus icon
-apt install -y papirus-icon-theme -t bullseye-backports
-
-# Papirus folder
-wget -qO- https://git.io/papirus-folders-install | sh
-papirus-folders -C yaru --theme Papirus-Dark
-
-# WhiteSur Icon Theme
-cd /home/$username
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-cd WhiteSur-icon-theme
-mkdir -p /home/$username/.local/share/icons
-./install.sh -d /home/$username/.local/share/icons
-cd .. && rm -rf WhiteSur-icon-theme
-cd builddir
-
-# Restricted extras
-apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
-
-# Alacritty Dependencies
-apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-
-# libreoffice install
-apt install -y libreoffice-gtk3 libreoffice-impress libreoffice-calc libreoffice-writer
-
-# libreoffice sifr theme
-curl -s https://raw.githubusercontent.com/rizmut/libreoffice-style-sifr/master/install-sifr.sh | sh
-
-# install build tools
-apt install -y build-essential linux-headers-$(uname -r)
-
-# install nvidia detect
-apt install -y nvidia-detect
 
 # setup starship
 wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
@@ -117,6 +37,9 @@ cp bash_aliases /home/$username/.bash_aliases
 
 # install login manager
 apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings slick-greeter lightdm-settings numlockx
+
+# own files
+chown -R $username:$username /home/$username
 
 echo 
 echo lxqt install complete, Reboot and Enjoy
