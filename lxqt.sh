41bec9b23b62963b66a1ea556e1143fbbc04bff6
ankit90s <ak7591527@gmail.com>
2023-04-06 15:37:49 +0530

    added network manager

--- lxqt.sh
+++ lxqt.sh
@@ -24,6 +24,9 @@ apt install -y exa
 # PDF reader of choice
 apt install -y evince
 
+# network manager
+apt install -y network-manager-gnome
+
 # setup starship
 wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf starship-x86_64-unknown-linux-musl.tar.gz
